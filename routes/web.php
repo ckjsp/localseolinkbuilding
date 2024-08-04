90b04bbdcd8cae52bc8ba0bedcdb7785b3c0819a
Nitikajsp <nitika@jspinfotech.com>
2024-08-04 22:36:27 +0530

    route conflict resolve

--- routes/web.php
+++ routes/web.php
@@ -69,17 +69,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
             Route::get('/advertiser/marketplace', 'marketplace')->name('advertiser.marketplace');
             Route::get('/advertiser/projects', 'projects')->name('advertiser.projects');
             Route::get('/advertiser/cart', 'cart')->name('advertiser.cart');
-<<<<<<< HEAD
-            Route::post('/advertiser/projects/store', 'projectStore')->name('advertiser.projects.store');
-            Route::get('/advertiser/menu', 'showMenu')->name('advertiser.menu');
-=======
             Route::get('/advertiser/projects/create', 'projectCreate')->name('advertiser.projects.create');
             Route::post('/advertiser/projects/add', 'projectStore')->name('advertiser.projects.store');
             Route::put('/advertiser/projects/{id}', 'projectUpdate')->name('advertiser.projects.update');
             Route::get('/advertiser/projects/{id}', 'update')->name('advertiser.projects.show');
             Route::post('/advertiser/menu', 'showMenu')->name('advertiser.menu');
-
->>>>>>> google-sign-in
         });
 
         Route::controller(PaymentController::class)->group(function () {
