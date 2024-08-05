e2db94128a3d6743bb930b697085f8c5cc999b1f
Nitikajsp <nitika@jspinfotech.com>
2024-08-05 18:29:03 +0530

    Start Guide Tour

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,13 @@
 @php $slug = isset($slug) ? $slug : ''; @endphp
 <!DOCTYPE html>
 
-<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset_url() }}" data-template="vertical-menu-template">
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset_url() }}" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>@yield('title')</title>
 
@@ -17,7 +19,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
 
     <!-- Icons -->
     <link rel="stylesheet" href="{{ asset_url('fonts/fontawesome.css') }}" />
@@ -37,9 +41,11 @@
     <link rel="stylesheet" href="{{ asset_url('libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}" />
     <link rel="stylesheet" href="{{ asset_url('libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}" />
 
+    <link rel="stylesheet" href="{{ asset('libs/shepherd/shepherd.css') }}">
+
     <!-- Page CSS -->
     @auth
-    <link rel="stylesheet" href="{{ asset_url('css/pages/cards-advance.css') }}" />
+        <link rel="stylesheet" href="{{ asset_url('css/pages/cards-advance.css') }}" />
     @endauth
 
     @stack('css')
@@ -55,39 +61,40 @@
     <script src="{{ asset_url('js/config.js') }}"></script>
     <script src="{{ asset_url('js/form-validation/jquery.validate.min.js') }}"></script>
     <script src="{{ asset_url('js/form-validation/additional-methods.min.js') }}"></script>
-    
+
 </head>
 
 <body>
     @auth
-    @if(Auth::user()->hasVerifiedEmail())
-    <!-- Layout wrapper -->
-    <div class="{{ (Auth::user()->role->name == 'Advertiser' && $slug != 'marketplace') ? 'layout-wrapper layout-navbar-full layout-horizontal layout-without-menu' : 'layout-wrapper layout-content-navbar' }}">
-        <div class="layout-container">
-            @if(Auth::user()->role->name == 'Advertiser')
-                @include('includes.second_navbar')
-            @else
-                @include('includes.navbar')
-            @endif
-            <!-- Layout container -->
-            <div class="layout-page">
-                <!-- Content wrapper -->
-                <div class="content-wrapper">
-                    @yield('content')
-                    @include('includes.footer')
+        @if(Auth::user()->hasVerifiedEmail())
+            <!-- Layout wrapper -->
+            <div
+                class="{{ (Auth::user()->role->name == 'Advertiser' && $slug != 'marketplace') ? 'layout-wrapper layout-navbar-full layout-horizontal layout-without-menu' : 'layout-wrapper layout-content-navbar' }}">
+                <div class="layout-container">
+                    @if(Auth::user()->role->name == 'Advertiser')
+                        @include('includes.second_navbar')
+                    @else
+                        @include('includes.navbar')
+                    @endif
+                    <!-- Layout container -->
+                    <div class="layout-page">
+                        <!-- Content wrapper -->
+                        <div class="content-wrapper">
+                            @yield('content')
+                            @include('includes.footer')
+                        </div>
+                        <!-- Content wrapper -->
+                    </div>
+                    <!-- / Layout page -->
                 </div>
-                <!-- Content wrapper -->
-            </div>
-            <!-- / Layout page -->
-        </div>
 
-        <!-- Overlay -->
-        <div class="layout-overlay layout-menu-toggle"></div>
-        <!-- Drag Target Area To SlideIn Menu On Small Screens -->
-        <div class="drag-target"></div>
-    </div>
-    <!-- / Layout wrapper -->
-    @endif
+                <!-- Overlay -->
+                <div class="layout-overlay layout-menu-toggle"></div>
+                <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+                <div class="drag-target"></div>
+            </div>
+            <!-- / Layout wrapper -->
+        @endif
     @endauth
     @yield('auth-content')
     <!-- Core JS -->
@@ -107,21 +114,23 @@
 
     <!-- Vendors JS -->
     <script src="{{ asset_url('libs/swiper/swiper.js') }}"></script>
+    <script src="{{ asset_url('libs/shepherd/shepherd.css') }}"></script>
 
     <!-- Main JS -->
     <script src="{{ asset_url('js/main.js') }}"></script>
     @auth
-    @if(Auth::user()->role->name == 'Advertiser')
-        <script>
-            $cartCookie = getCookie('cart')
-            if ($cartCookie != '') {
-                $cartArr = JSON.parse($cartCookie);
-                $('.nav-cart-icon').attr('data-item-count', $cartArr.length);
-            }
-        </script>
-        
-    @endif
+        @if(Auth::user()->role->name == 'Advertiser')
+            <script>
+                $cartCookie = getCookie('cart')
+                if ($cartCookie != '') {
+                    $cartArr = JSON.parse($cartCookie);
+                    $('.nav-cart-icon').attr('data-item-count', $cartArr.length);
+                }
+            </script>
+
+        @endif
     @endauth
     @stack('script')
 </body>
+
 </html>
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+const mix = require("laravel-mix");
+
+mix.js("resources/js/shepherd.js", "public/js"); // Add Shepherd.js compilation
