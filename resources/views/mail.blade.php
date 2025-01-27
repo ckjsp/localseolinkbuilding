bf4e573e07d125b274ff327fda2f45c425b0e570
miteshdalsaniya774 <miteshdalsaniya@jspinfotech.com>
2025-01-27 15:52:15 +0530

    cart page chnage radiyo button  javascript

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -8,11 +8,11 @@
 </head>
 
 <body>
-    <div class="main-section" style="width:40%;background-color:#1c8adb4a; margin:auto; text-align:center; box-shadow:0px 0px 35px #bfbfbf; margin:10vh auto;padding-bottom:30px;">
+    <div class="main-section">
         <!-- <div class="header-section" style="background: #f1f1f1;">
             <img src="{{ asset_url('img/logo.png') }}" class="logo" style="width: 10vw;" alt="Logo">
         </div> -->
-        <div class="content-section" style="width: 50%; margin: auto; text-align: start;">
+        <div class="content-section">
             {!! $customData['msg'] !!}
         </div>
         <div class="footer-section"></div>
